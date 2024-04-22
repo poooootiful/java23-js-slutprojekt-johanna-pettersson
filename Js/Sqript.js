0e181bedf5d0add7e781e26b5fb4e334029b964b
poooootiful <johanna_pettersson@live.com>
2024-04-22 23:19:30 +0200

    test

--- Js/Sqript.js
+++ Js/Sqript.js
@@ -1,38 +1,10 @@
 const Apikey = "6e68e83df2f02eec821aa01c5ae4ab83";
 const ReadTocken ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTY4ZTgzZGYyZjAyZWVjODIxYWEwMWM1YWU0YWI4MyIsInN1YiI6IjY2MWY5YTI1NTI4YjJlMDE3ZDQwNzA3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6DLfm6K5i7UuJGRxlYQpFhJ5Syd65EOn4JEQsjO-HE4";
-     
-let searchterm = "Spider"
 
-function getresults() {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer .eyJhdWQiOiI2ZTY4ZTgzZGYyZjAyZWVjODIxYWEwMWM1YWU0YWI4MyIsInN1YiI6IjY2MWY5YTI1NTI4YjJlMDE3ZDQwNzA3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6DLfm6K5i7UuJGRxlYQpFhJ5Syd65EOn4JEQsjO-HE4'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/search/movie?query=spider&include_adult=false&language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+function movieresult() {
+    var x = document.getElementById("MovieSearch");
+    console.log(x);
+    var test = x.getAttribute ("value")
+    console.log(test);
 }
 
-//Getmovie ();
-
-//function Getmovie (textinput) {
-  /*const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTY4ZTgzZGYyZjAyZWVjODIxYWEwMWM1YWU0YWI4MyIsInN1YiI6IjY2MWY5YTI1NTI4YjJlMDE3ZDQwNzA3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6DLfm6K5i7UuJGRxlYQpFhJ5Syd65EOn4JEQsjO-HE4'
-    }
-  };**/
-  
-  /**fetch('https://api.themoviedb.org/3/search/movie?query=spider&include_adult=false&language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-  const url ="";**/
-
-//};
